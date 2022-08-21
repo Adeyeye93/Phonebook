6783520665b524a5382c2e3ed5a2567bd0c0251f
Adeyeye Seyi <97676173+Adeyeye93@users.noreply.github.com>
2022-08-21 19:13:18 +0100

    first commit7

--- phonebook/phonebook/settings.py
+++ phonebook/phonebook/settings.py
@@ -124,3 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
